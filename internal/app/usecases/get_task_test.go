4972b6e801d47d6e3e0b6c75d6886b8bf4503c5c
Amirhossein <38152308+Amirhossein2000@users.noreply.github.com>
2023-12-16 23:27:02 +0100

    Add TestGetTaskUseCase_Execute

---
+++ internal/app/usecases/get_task_test.go
@@ -0,0 +1,109 @@
+package usecases
+
+import (
+	"errors"
+	"math/rand"
+	"testing"
+
+	"RequestTasker/internal/app/services/logger"
+	"RequestTasker/internal/domian/common"
+	"RequestTasker/internal/domian/entities"
+	"RequestTasker/internal/mocks"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetTaskUseCase_Execute(t *testing.T) {
+	Convey("GetTaskUseCase.Execute()", t, func() {
+		logger := logger.NewLogger()
+		taskRepository := mocks.NewTaskRepositoryMock(t)
+		taskStatusRepository := mocks.NewTaskStatusRepositoryMock(t)
+		taskResultRepository := mocks.NewTaskResultRepositoryMock(t)
+		getTaskUseCase := NewGetTaskUseCase(
+			logger,
+			taskRepository,
+			taskStatusRepository,
+			taskResultRepository,
+		)
+
+		expectedErr := errors.New("expectedErr")
+		newTask := entities.NewTask("url", "method", nil, nil)
+		expectedTask := entities.BuildTask(
+			rand.Int63(),
+			newTask.CreatedAt(),
+			newTask.PublicID(),
+			newTask.Url(),
+			newTask.Method(),
+			newTask.Headers(),
+			newTask.Body(),
+		)
+		expectedStatus := entities.NewTaskStatus(expectedTask.ID(), common.StatusNEW)
+
+		Convey("When taskRepository.GetByPublicID() returns error", func() {
+			taskRepository.
+				On("GetByPublicID", expectedTask.PublicID()).
+				Return(entities.Task{}, expectedErr)
+
+			_, _, _, err := getTaskUseCase.Execute(expectedTask.PublicID())
+			So(err, ShouldEqual, common.InternalError)
+		})
+
+		Convey("When taskRepository.GetByPublicID() works", func() {
+			taskRepository.
+				On("GetByPublicID", expectedTask.PublicID()).
+				Return(expectedTask, nil)
+
+			Convey("When taskStatusRepository.GetLatestByTaskID() returns error", func() {
+				taskStatusRepository.
+					On("GetLatestByTaskID", expectedTask.ID()).
+					Return(expectedStatus, expectedErr)
+
+				_, _, _, err := getTaskUseCase.Execute(expectedTask.PublicID())
+				So(err, ShouldEqual, common.InternalError)
+			})
+
+			Convey("When taskStatusRepository.GetLatestByTaskID() returns status without result", func() {
+				taskStatusRepository.
+					On("GetLatestByTaskID", expectedTask.ID()).
+					Return(expectedStatus, nil)
+
+				task, status, _, err := getTaskUseCase.Execute(expectedTask.PublicID())
+				So(err, ShouldBeNil)
+				So(task, ShouldResemble, &expectedTask)
+				So(status, ShouldResemble, &expectedStatus)
+			})
+
+			Convey("When taskStatusRepository.GetLatestByTaskID() returns status with result", func() {
+				expectedStatus := entities.NewTaskStatus(expectedTask.ID(), common.StatusDONE)
+				expectedResult := entities.NewTaskResult(
+					expectedTask.ID(),
+					200,
+					nil,
+					10,
+				)
+				taskStatusRepository.On("GetLatestByTaskID", expectedTask.ID()).Return(expectedStatus, nil)
+
+				Convey("When taskResultRepository.GetByTaskID() returns error", func() {
+					taskResultRepository.
+						On("GetByTaskID", expectedTask.ID()).
+						Return(entities.TaskResult{}, expectedErr)
+
+					_, _, _, err := getTaskUseCase.Execute(expectedTask.PublicID())
+					So(err, ShouldEqual, common.InternalError)
+				})
+
+				Convey("When taskResultRepository.GetByTaskID() works", func() {
+					taskResultRepository.
+						On("GetByTaskID", expectedTask.ID()).
+						Return(expectedResult, nil)
+
+					task, status, result, err := getTaskUseCase.Execute(expectedTask.PublicID())
+					So(err, ShouldBeNil)
+					So(task, ShouldResemble, &expectedTask)
+					So(status, ShouldResemble, &expectedStatus)
+					So(result, ShouldResemble, &expectedResult)
+				})
+			})
+		})
+	})
+}
